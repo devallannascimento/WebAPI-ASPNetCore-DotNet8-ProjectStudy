5a472b1fc28598161357d5584517ef97425a49c8
devallannascimento <devallannascimento@gmail.com>
2024-08-22 13:35:36 -0300

    implementação do JWT RefreshToken

--- APICatalogo/APICatalogo/Services/TokenService.cs
+++ APICatalogo/APICatalogo/Services/TokenService.cs
@@ -40,11 +40,48 @@ public class TokenService : ITokenService
 
     public string GenerateRefreshToken()
     {
-        throw new NotImplementedException();
+        var secureRadomBytes = new byte[128];
+
+        using var randomNumberGenerator = RandomNumberGenerator.Create();
+
+        randomNumberGenerator.GetBytes(secureRadomBytes);
+
+        var refreshToken = Convert.ToBase64String(secureRadomBytes);
+
+        return refreshToken;
     }
 
     public ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration _config)
     {
-        throw new NotImplementedException();
+        var secretKey = _config["JWT:SecretKey"] ?? throw new InvalidOperationException("Invalid key");
+
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+            ValidateLifetime = false
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var principal = tokenHandler.ValidateToken(
+                token,
+                tokenValidationParameters,
+                out SecurityToken securityToken
+            );
+
+        if ( securityToken is not JwtSecurityToken jwtSecurityToken || 
+            !jwtSecurityToken.Header.Alg.Equals(
+                SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase
+                )
+            )
+        {
+            throw new InvalidOperationException("Invalid token");
+        }
+
+        return principal;
     }
 }
